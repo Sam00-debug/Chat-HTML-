ed2aaacf460525d307e03c7e57c97996c772e704
Sam00-debug <umeshjamre321@gmail.com>
2026-08-11 15:15:30 +0530

    Update wallpapers.js

--- wallpapers.js
+++ wallpapers.js
@@ -1,15 +1,14 @@
 const wallpapers = [
-  "https://ac-o.arca.live/20241207sac/6f1fe2e5380dc0c55eda8dea0dc80bbc46d1126152add76b45245aa418134320.jpg?expires=1786399361&key=wOfpu_qPNlgWpvFzLy12-w&type=orig",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeNz9-TQzEhBAjMdlqMfJc_LY5WofS89gtIz0Y85JeiXZ3wgHN5baX--5T&s=10",
 
-  "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/b/b6/Guilty_%28Wave_of_Disbelief%29_cover_idle.png/revision/latest/scale-to-width-down/985?cb=20240926034038",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-mtZDtHSDNE4AarNHmTrKei80zuwMMpOj2xw160QUIskokQjh87V1FRhW&s=10",
 
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLzngNK0aFLX14FqnfCLUX4cGHef2RSlRHcS4k4rFvRA&s=10",
 
   "https://pbs.twimg.com/media/HNHe9JnbUAArm4X.jpg",
 
   "https://pbs.twimg.com/media/HL-RU0va0AAYHRg.jpg",
-
-  "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/5/5d/Beauty_Full_Shot_Event_Title.png/revision/latest/scale-to-width-down/985?cb=20240704123421",
+ 
 
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXPFVWM6Ag9Hsj5QTC1rpDMp6LAEqt7v9zdIG97IPi2GqXW_To6epERLc&s=10",
 
