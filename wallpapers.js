61009fe99b88bd98445156105eb50e86061ee1db
Sam00-debug <umeshjamre321@gmail.com>
2026-08-11 14:39:27 +0530

    Create wallpapers.js

---
+++ wallpapers.js
@@ -0,0 +1,19 @@
+const wallpapers = [
+  "https://ac-o.arca.live/20241207sac/6f1fe2e5380dc0c55eda8dea0dc80bbc46d1126152add76b45245aa418134320.jpg?expires=1786399361&key=wOfpu_qPNlgWpvFzLy12-w&type=orig",
+
+  "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/b/b6/Guilty_%28Wave_of_Disbelief%29_cover_idle.png/revision/latest/scale-to-width-down/985?cb=20240926034038",
+
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLzngNK0aFLX14FqnfCLUX4cGHef2RSlRHcS4k4rFvRA&s=10",
+
+  "https://pbs.twimg.com/media/HNHe9JnbUAArm4X.jpg",
+
+  "https://pbs.twimg.com/media/HL-RU0va0AAYHRg.jpg",
+
+  "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/5/5d/Beauty_Full_Shot_Event_Title.png/revision/latest/scale-to-width-down/985?cb=20240704123421",
+
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXPFVWM6Ag9Hsj5QTC1rpDMp6LAEqt7v9zdIG97IPi2GqXW_To6epERLc&s=10",
+
+  "https://pbs.twimg.com/media/G-WPuy9XEAAztY7.jpg",
+
+  "https://w.wallhaven.cc/full/m3/wallhaven-m32pdy.jpg"
+];
